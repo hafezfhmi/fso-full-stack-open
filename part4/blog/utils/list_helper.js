fb1c039d5cd730e53c1a4762d523d57084e28c57
hafezfhmi <hafezfahmi2000@gmail.com>
2022-04-04 17:08:17 +0800

    Done Exercise 4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,4 +6,12 @@ const totalLikes = (blogs) => {
   return blogs.reduce((accumulator, current) => accumulator + current.likes, 0);
 };
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((acc, curr) =>
+    acc.likes < curr.likes
+      ? { title: curr.title, author: curr.author, likes: curr.likes }
+      : acc
+  );
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog };
