23c832d550a830465674ddd926aa23a134eff4e1
hafezfhmi <hafezfahmi2000@gmail.com>
2022-04-27 01:04:14 +0800

    Done Exercise 4.15

---
+++ part4/blog/controllers/users.js
@@ -0,0 +1,37 @@
+const bcrypt = require('bcrypt');
+const usersRouter = require('express').Router();
+const User = require('../models/user');
+
+usersRouter.get('/', async (req, res, next) => {
+  try {
+    const users = await User.find({});
+
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.post('/', async (req, res, next) => {
+  try {
+    const { username, name, password } = req.body;
+
+    const saltRounds = 10;
+
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+
+    const user = new User({
+      username,
+      name,
+      password: passwordHash,
+    });
+
+    const savedUser = await user.save();
+
+    res.status(201).json(savedUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = usersRouter;
