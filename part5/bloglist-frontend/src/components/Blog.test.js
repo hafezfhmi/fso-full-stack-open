b024607764173fe1a789e38c857161ee7e15d327
hafezfhmi <hafezfahmi2000@gmail.com>
2022-06-15 01:16:21 +0800

    Done Exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,44 +1,68 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
 
-test('Blog component render blog title and author but not url and likes by default', () => {
+describe('Testing blog component where:', () => {
   const blog = {
     title: 'Jack Adventure',
     author: 'John Doe Author',
     url: 'johndoe.com',
     likes: 2,
+    user: { name: 'John Doe Owner', username: 'JohnDoe' },
   };
 
-  /*
-  GETTING ELEMENT BY CSS CLASS
+  const user = {
+    username: 'JohnDoe',
+  };
 
-  const { container } = render(<Blog blog={blog} />);
+  test('Blog component render blog title and author but not url and likes by default', () => {
+    /*
+    GETTING ELEMENT BY CSS CLASS
+  
+    const { container } = render(<Blog blog={blog} />);
+  
+    const div = container.querySelector('.blog');
+    expect(div).toHaveTextContent(blog.title);
+    expect(div).toHaveTextContent(blog.author);
+    */
 
-  const div = container.querySelector('.blog');
-  expect(div).toHaveTextContent(blog.title);
-  expect(div).toHaveTextContent(blog.author);
-  */
+    render(<Blog blog={blog} />);
 
-  render(<Blog blog={blog} />);
+    /*
+    PRINT THE ENTIRE RENDERED ELEMENT TO CONSOLE
+    screen.debug();
+    */
 
-  /*
-  PRINT THE ENTIRE RENDERED ELEMENT TO CONSOLE
-  screen.debug();
-  */
+    let element = screen.getByText(`${blog.title} ${blog.author}`);
+    expect(element).toBeDefined();
 
-  let element = screen.queryByText(`${blog.title} ${blog.author}`);
-  expect(element).toBeDefined();
+    /*
+    PRINT THE TARGETED ELEMENT TO CONSOLE
+    screen.debug(element);
+    */
 
-  /*
-  PRINT THE TARGETED ELEMENT TO CONSOLE
-  screen.debug(element);
-  */
+    element = screen.queryByText(blog.url);
+    expect(element).toBeNull();
 
-  element = screen.queryByText(blog.url);
-  expect(element).toBeNull();
+    element = screen.queryByText(blog.likes);
+    expect(element).toBeNull();
+  });
 
-  element = screen.queryByText(blog.likes);
-  expect(element).toBeNull();
+  test('Blog url and likes are shown when view button is clicked', async () => {
+    render(<Blog blog={blog} user={user} />);
+
+    const userClick = userEvent.setup();
+
+    const button = screen.getByText('view');
+
+    await userClick.click(button);
+
+    let element = screen.getByText(blog.url);
+    expect(element).toBeDefined();
+
+    element = screen.getByText(blog.likes, { exact: false });
+    expect(element).toBeDefined();
+  });
 });
