3700d9761d7a39396ba276d17996403e6936c0f1
hafezfhmi <hafezfahmi2000@gmail.com>
2022-06-19 22:14:56 +0800

    Done Exercise 5.18

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,11 +1,41 @@
 describe("Blog app", function () {
   beforeEach(function () {
+    // Reset db to start test with a "default" db
     cy.request("POST", "http://localhost:3003/api/testing/reset");
+
+    // Create new user
+    const user = {
+      username: "admin",
+      password: "abc123",
+      name: "admin",
+    };
+    cy.request("POST", "http://localhost:3003/api/users/", user);
+
+    // Visit the page to test
     cy.visit("http://localhost:3000");
   });
 
+  // Test if login form is shown by default
   it("Login form is shown", function () {
     cy.contains("log in to application");
     cy.contains("Login").click();
+  });
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.get("#username").type("admin");
+      cy.get("#password").type("abc123");
+      cy.get("#login-button").click();
+
+      cy.contains("Logged in");
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.get("#username").type("abc");
+      cy.get("#password").type("abc");
+      cy.get("#login-button").click();
+
+      cy.contains("wrong username or password");
+    });
   });
 });
